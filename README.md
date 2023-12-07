1ea71340b64db9d719caa179f62fc3d56869d57c
NgocNguyen1511 <125537092+NgocNguyen1511@users.noreply.github.com>
2023-12-08 02:17:18 +0700

    doi_moi

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# weather_app
+# weather_lastest
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/weather_expanded.dart';
-import 'package:weather_app/weather_plus.dart';
-import 'package:weather_app/weather_temperature.dart';
+import '../../../BaiAPI/WeatherApp-main/lib/Screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,118 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: const Color(0xFF5D50FE),
-        child: Center(
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                bottom: 150,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset("assets/images/trang.png"),
-                  ],
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    width: 414,
-                    height: 240,
-                    color: Color(0xFF5C2AFF),
-                  ),
-                ],
-              ),
-              Positioned(
-                bottom: 220,
-                right: 12,
-                child: Column(
-                  children: <Widget>[Image.asset("assets/images/nen_mo.png")],
-                ),
-              ),
-              Positioned(
-                bottom: 220,
-                right: 12,
-                child: Column(
-                  children: <Widget>[Image.asset("assets/images/nen_giua.png")],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: InkWell(
-                  onTap: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const weather_temperature(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: 180,
-                    height: 59,
-                    margin: const EdgeInsets.all(70),
-                    decoration: BoxDecoration(
-                        color: Color(0xFFFFFFFF),
-                        borderRadius: BorderRadius.circular(50)),
-                    child: const Align(
-                      alignment: Alignment.center,
-                      child:  Text(
-                        "EXPLORE",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 230,
-                left: 0,
-                right: 50,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset("assets/images/sun.png"),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 240,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset("assets/images/cloud.png"),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-
+    return const MaterialApp(
+      title: 'WeatherApp',
+      home: HomeScreenApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
